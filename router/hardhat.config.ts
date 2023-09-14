c39fbea74e6a489dc305ea7256b378eda02854d3
Pedro Brandão <pgbrandao@gmail.com>
2023-09-14 19:35:25 +0100

    Adjusting hardhat provider

--- router/hardhat.config.ts
+++ router/hardhat.config.ts
@@ -9,7 +9,7 @@ const config: HardhatUserConfig = {
   networks: {
     hardhat: {
       forking: {
-        url: `https://polygon-mainnet.infura.io/v3/${process.env.INFURA_KEY}`,
+        url: `https://polygon-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_KEY}`,
       },
     },
   },
