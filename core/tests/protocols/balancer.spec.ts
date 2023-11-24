b0cb10e049d5c34487be21e2ec561d66c791f67f
Pedro Brandão <pgbrandao@gmail.com>
2023-11-24 18:21:45 +0000

    Updating balancer test and asset

--- core/tests/protocols/balancer.spec.ts
+++ core/tests/protocols/balancer.spec.ts
@@ -9,3 +9,12 @@ test("generateTransaction: USDC to jBRL/BRZ (balancerDeposit)", async () => {
     amountIn: "1000000000",
   });
 });
+
+test("generateTransaction: maticX/WMATIC (balancerDeposit) to USDC", async () => {
+  await simulateSingleAssetToSingleAsset({
+    chainId: 137,
+    inputAssetId: "9b09afe5-c740-4cd7-a247-4fe4950a7f33",
+    outputAssetId: "e251ecf6-48c2-4538-afcd-fbb92424054d",
+    amountIn: "10000000000000000000000",
+  });
+});
--- core/tests/simulation/find-slots.spec.ts
+++ core/tests/simulation/find-slots.spec.ts
@@ -9,28 +9,7 @@ test("findSlots", async () => {
 
   console.log(
     await findSlots({
-      asset: assetStore.getAssetById("371b83f1-3301-4c69-b3ad-8d199c6d1774"),
-      provider,
-    })
-  );
-
-  console.log(
-    await findSlots({
-      asset: assetStore.getAssetById("46062b41-6661-4eae-bc11-4d4ec07b062f"),
-      provider,
-    })
-  );
-
-  console.log(
-    await findSlots({
-      asset: assetStore.getAssetById("03f36f17-bbc2-4d8d-b0b2-9ce0f534d708"),
-      provider,
-    })
-  );
-
-  console.log(
-    await findSlots({
-      asset: assetStore.getAssetById("33b90e57-ded7-4da9-92c9-6d3ed0f1c53d"),
+      asset: assetStore.getAssetById("9b09afe5-c740-4cd7-a247-4fe4950a7f33"),
       provider,
     })
   );
